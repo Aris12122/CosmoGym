e8b1d7a2a438d1c3b19e32b44f12ae3f6fdf9294
Aris12122 <miha.tropin@gmail.com>
2022-07-25 21:54:18 +0300

    contest Арифметика 1

---
+++ conspects/Введение/Основные понятия/Арифметика/contest/arifm-formula-2/v.cpp
@@ -0,0 +1,18 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+
+
+int main(int argc, char* argv[]) {
+    registerValidation(argc, argv);
+
+    inf.readInt(1, 10'000, "x");
+    inf.readSpace();
+    inf.readInt(, "y");
+
+    inf.readEoln();
+    inf.readEof();
+}
